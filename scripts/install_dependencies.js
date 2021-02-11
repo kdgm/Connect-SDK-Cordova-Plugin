0850dc2962f70ccfad8301ed29655082dbdf36d3
Wouter <Bin>
2021-02-11 11:58:51 +0100

    fix requireCordovaModule for non cordova modules is not allowed anymore.

--- scripts/install_dependencies.js
+++ scripts/install_dependencies.js
@@ -1,7 +1,7 @@
-module.exports = function (context) {
-     var shell = context.requireCordovaModule('shelljs');
+const exec = require('child_process').exec;
 
+module.exports = function (ctx) {
      console.log("Installing node dependencies for cordova-plugin-connectsdk");
-     shell.cd(context.opts.plugin.dir);
-     shell.exec('npm install');
+     exec("cd", ctx.opts.plugin.dir);
+     exec("npm", "install");
  };
