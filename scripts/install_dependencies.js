ea8f5c08039834e8e561c8ab362f40b0892c64d1
Wouter <Bin>
2021-02-11 15:03:37 +0100

    execute both steps at once, otherwise they will be executed in a separate shell.

--- scripts/install_dependencies.js
+++ scripts/install_dependencies.js
@@ -1,7 +1,6 @@
 const exec = require('child_process').exec;
 
 module.exports = function (ctx) {
-     console.log("Installing node dependencies for cordova-plugin-connectsdk");
-     exec("cd", ctx.opts.plugin.dir);
-     exec("npm", "install");
+    console.log("Installing node dependencies for cordova-plugin-connectsdk");
+    exec(`cd ${ctx.opts.plugin.dir} && npm install`);
  };
